5b2804e8a68aef7eb23952c3b28791e08ecafb91
Rafed Muhammad Yasir <rafed123@yahoo.com>
2019-12-19 12:05:42 +0600

    Added tesseract maj versions

--- releases.py
+++ releases.py
@@ -3,10 +3,12 @@ import json
 
 tags = []
 
-for i in range (1, 4): # 24
+# https://api.github.com/repos/tesseract-ocr/tesseract/tags
+
+for i in range (1, 3): 
     print("Doing", i)
 
-    url = "https://api.github.com/repositories/5108051/tags?page={}".format(i)
+    url = "https://api.github.com/repositories/22887094/tags?page={}".format(i)
 
     r = requests.get(url)
     tag_page = json.loads(r.text)
@@ -18,8 +20,3 @@ for tag in tags:
     print("{},{}".format(tag["name"], tag["commit"]["sha"]))
 
 print(len(tags))
-# print(json.dumps(tags, indent=2))
-# print(len(tags))
-
-# with open('data.txt', 'w') as outfile:
-#     json.dump(issues, outfile)
